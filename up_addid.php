2c76bb64e7e68716235ff18001b1812512539a9b
gitsdev <sudipto126@gmail.com>
2017-03-27 00:42:47 +0530

    Add files via upload

--- up_addid.php
+++ up_addid.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(E_ALL);
 
+
 	$serverName = "jgdbserver001.cdgdaha6zllk.us-west-2.rds.amazonaws.com"; //serverName\instanceName
 	$conn = mssql_connect($serverName, 'liveuser', 'JGLive@538%');
 	mssql_select_db("JGBS",$conn);
-	/*mssql_select_db("JGC",$conn);*/
 	if( $conn === false ) {	die( print_r( mssql_error(), true)); }
 	
 	
