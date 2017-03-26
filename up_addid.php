e6f639e5991b17045825fa6fb4797c8c65ca8dd3
gitsdev <sudipto126@gmail.com>
2017-03-27 01:53:43 +0530

    Add files via upload

--- up_addid.php
+++ up_addid.php
@@ -10,16 +10,8 @@
 	mssql_select_db("JGBS",$conn);
 	if( $conn === false ) {	die( print_r( mssql_error(), true)); }
 	
-	// Send a select query to MSSQL
-$query = mssql_query('SELECT AddedByUserID FROM dbo.tblInstallUsers');
 
-// Print the field type and length
-echo '\'' . mssql_field_name($query, 0) . '\' is a type of ' . 
-     strtoupper(mssql_field_type($query, 0)) . 
-     '(' . mssql_field_length($query, 0) . ')';
-	
-	
-	echo $sql="UPDATE dbo.tblInstallUsers SET AddedByUserID=AddedByUserID|4 WHERE Id='2847'";
+	echo $sql="UPDATE dbo.tblInstallUsers SET AddedByUserID = 4 WHERE Id='2847'";
 	echo "ssq";
 	$query = mssql_query($sql);
 	
