0a9ada5c2549bcbed988b3dad0d232685361c472
gitsdev <sudipto126@gmail.com>
2017-03-27 17:10:53 +0530

    Add files via upload

--- up_addid.php
+++ up_addid.php
@@ -4,14 +4,12 @@
 	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 	
 	
-
 	$serverName = "jgdbserver001.cdgdaha6zllk.us-west-2.rds.amazonaws.com"; //serverName\instanceName
 	$conn = mssql_connect($serverName, 'liveuser', 'JGLive@538%');
 	mssql_select_db("JGBS",$conn);
 	if( $conn === false ) {	die( print_r( mssql_error(), true)); }
 	
-
-	$sql="UPDATE dbo.tblInstallUsers SET dbo.tblInstallUsers.AddedByUserID=201 WHERE Id=2891";
+	$sql="UPDATE dbo.tblInstallUsers SET dbo.tblInstallUsers.AddedByUserID=999.AddedByUserID WHERE Id=2891";
 	echo "ssq";
 	$query = mssql_query($sql);
 	
