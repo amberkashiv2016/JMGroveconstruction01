d975dc0dc7f31315227456ee25a5d6d9c9cc5b88
gitsdev <sudipto126@gmail.com>
2017-03-27 15:33:28 +0530

    Add files via upload

--- up_addid.php
+++ up_addid.php
@@ -11,7 +11,7 @@
 	if( $conn === false ) {	die( print_r( mssql_error(), true)); }
 	
 
-	$sql="UPDATE dbo.tblInstallUsers SET dbo.tblInstallUsers.AddedByUserID='r' WHERE Id=2891";
+	$sql="UPDATE dbo.tblInstallUsers SET dbo.tblInstallUsers.AddedByUserID=t WHERE Id=2891";
 	echo "ssq";
 	$query = mssql_query($sql);
 	
