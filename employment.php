93dc6389fc5c3a84804663038e9623d14d44cf22
gitsdev <sudipto126@gmail.com>
2017-03-29 17:20:34 +0530

    Add files via upload

--- employment.php
+++ employment.php
@@ -4,8 +4,8 @@
 $serverName = "jgdbserver001.cdgdaha6zllk.us-west-2.rds.amazonaws.com"; //serverName\instanceName
 $connectionInfo = array( "Database"=>"JGBS_Dev", "UID"=>"devloperuser", "PWD"=>"JG%987");
 $conn = mssql_connect($serverName, 'liveuser', 'JGLive@538%');
-/*mssql_select_db("JGBS",$conn);*/
-mssql_select_db("JGC",$conn);
+mssql_select_db("JGBS",$conn);
+/*mssql_select_db("JGC",$conn);*/
 if( $conn === false ) {
     die( print_r( sqlsrv_errors(), true));
 }
