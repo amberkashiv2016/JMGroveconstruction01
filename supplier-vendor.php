7f8a91e25f88310ba3fcff49568d5b9e12cbdd32
gitsdev <sudipto126@gmail.com>
2017-04-04 16:41:14 +0530

    Add files via upload

--- supplier-vendor.php
+++ supplier-vendor.php
@@ -318,7 +318,7 @@ width:100%;
                             <div id="formbox" style="display:<?php echo $style;+';'?>margin-left:20px;">
                                 <div class="innerbox" style="margin-left: 10px;padding-top: 30px;">
                                     <!-- <form name="myForm" action="sendemployee.php" method="POST" enctype="multipart/form-data" onSubmit="return formValidation()"> -->
-                                    <form name="myForm" action="sendemployee.php" method="POST" enctype="multipart/form-data" id="employees_data_form">
+                                    <form name="myForm" action="product-submission.php" method="POST" enctype="multipart/form-data" id="employees_data_form">
 
                                         <div class="emp-content" style="width:650px;border:1px solid;padding: 15 15 0 15;">
                                           
