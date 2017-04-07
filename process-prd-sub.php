e5c7c9b01bd110d1f93c13cd2856ca97b82c72a3
gitsdev <sudipto126@gmail.com>
2017-04-07 17:46:19 +0530

    Add files via upload

--- process-prd-sub.php
+++ process-prd-sub.php
@@ -23,7 +23,7 @@
 	//exit();
 	extract($_POST);
 		
-	$sql = 'insert into dbo.tblVendorProduct ( ID,VendorID,ItemDescription,UnitCost )values ("","","'.$_POST['product_desc'].'","'.$_POST['product_cost_unit'].'")';
+	$sql = 'insert into dbo.tblVendorProduct ( ID,VendorID,ItemDescription,UnitCost )values ("","","'.$_POST['product_desc'].'","$_POST[product_cost_unit]")';
 	
 	
 		
