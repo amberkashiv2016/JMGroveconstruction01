58d988355979ec136d4ae2c91ef2482a3159b667
gitsdev <sudipto126@gmail.com>
2017-04-14 16:34:02 +0530

    Sudipto - Task ID #426# -2017-04-14 4:30pm

--- process-prd-sub.php
+++ process-prd-sub.php
@@ -15,46 +15,14 @@
 	date_default_timezone_set("Asia/Kolkata");
 	$now=date("YmdHis"); //echo $now;
 	$now_dt=date("Y-m-d");
+	
+
+	if($_POST['hid_pr_submit']=='hid_pr_submit_val')
+	{
+	//print_r($_POST);
+	//exit();
 	extract($_POST);
 	
-	/*print_r($_POST);
-	exit();*/
-	
-	$con_loop=0;
-		
-		if(count($s2)>0)
-		{
-		
-			foreach($_POST['product_name'] as $contact_info)
-			{
-			if(trim($product_name[$con_loop])!='')
-				{
-					if($contact_info!='')
-					{
-					/*$venid==$vendorid[$con_loop];
-					$pdesc=$product_desc[$con_loop];
-					$pcunit=$product_cost_unit[$con_loop];*/
-					
-					/*$last_pid_qr = mssql_fetch_array(mssql_query("SELECT MAX(ID) as pid FROM tblVendorProduct"));
-					$last_pid =	$last_pid_qr['pid']+1;*/
-											
-					mssql_query("insert into dbo.tblVendorProduct ( ID,VendorID,ItemDescription,UnitCost )values (7,1618,'test',3)");
-						break;
-					}
-				
-				}
-			$con_loop++;
-			}
-	
-	
-		}
-	
-	/*if($_POST['hid_pr_submit']=='hid_pr_submit_val')
-	{
-			
-	
-	
-	
-	
-	}*/
+	print_r($_POST);
+	}
 	?>
