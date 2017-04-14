da89317b8e9b7e88d49e43bf632bc9c49c91fa98
gitsdev <sudipto126@gmail.com>
2017-04-14 17:14:18 +0530

    Sudipto - Task ID #426# -2017-04-14 5:00pm

--- product-submission.php
+++ product-submission.php
@@ -367,7 +367,7 @@ width:100%;
 														  <td>USD</td>
 														</tr>
 														<tr>
-														  <td height="30" align="right">Upload image</td>
+														  <td height="30" align="right">Upload images</td>
 														  <td align="right"><!--<label class="fileContainer">Upload image<input type="file" name="product_image"/></label>--> <input type="text" name="product_image" style="text-transform: uppercase; padding: 3px 52px; line-height: 32px;" ></td>
 														 <td>Format JPEG - 5MB/Product</td>
 														  </tr>
