f9f452383d63b4402d11844fe661cc177c3b10d8
gitsdev <sudipto126@gmail.com>
2017-04-17 18:35:53 +0530

    Sudipto - Task ID #549# -2017-04-17 6:30pm

--- product-submission.php
+++ product-submission.php
@@ -1060,11 +1060,6 @@ width:100%;
                     }
                 }
 				
-				//alert(sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase().size);
-                
-				alert(arrInputs[i].files[i].size);
-				
-				
                 if (!blnValid) {
                     alert("Sorry, " + sFileName + " is invalid, allowed extensions are: " + _validFileExtensions.join(", "));
                     return false;
@@ -1082,12 +1077,55 @@ width:100%;
 			
         }
     }
+	
+	//var x = document.getElementsByClassName( 'prdimgcls' );
+    var txt = "";
+    if ('files' in arrInputs) {
+        if (arrInputs.files.length == 0) {
+            txt = "Select one or more files.";
+        } else {
+            for (var i = 0; i < arrInputs.files.length; i++) {
+                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+                var file = arrInputs.files[i];
+                if ('name' in file) {
+                    txt += "name: " + file.name + "<br>";
+                }
+                if ('size' in file) {
+                alert(x.files[0].size);
+                    txt += "size: " + file.size + " bytes <br>";
+                }
+            }
+        }
+    } 
   
     return false;
 			}
 			
 			
-		
+		function myFunction(){
+    var x = document.getElementsByClassName( 'prdimgcls' );
+    var txt = "";
+    if ('files' in x) {
+        if (x.files.length == 0) {
+            txt = "Select one or more files.";
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+                var file = x.files[i];
+                if ('name' in file) {
+                    txt += "name: " + file.name + "<br>";
+                }
+                if ('size' in file) {
+                alert(x.files[0].size);
+                    txt += "size: " + file.size + " bytes <br>";
+                }
+            }
+        }
+    } 
+    
+    
+	return false;
+}
 		
 		
 		
