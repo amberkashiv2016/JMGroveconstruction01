815a33c7d93104d9485a0a391c1068b244a0086a
Naveen <netcollab@users.noreply.github.com>
2017-09-20 13:33:30 +0530

    cron test script updated

--- filecheckstest.php
+++ filecheckstest.php
@@ -32,7 +32,10 @@
 		$dir_resume = "/var/www/www.jmgroveconstruction.com/demo/Resumes";
 	}
 	else{
-		$dsn = "Driver={ODBC Driver 11 for SQL Server};Server=$serverName;Database=$database;";
+		//$dsn = "Driver={ODBC Driver 11 for SQL Server};Server=$serverName;Database=$database;";
+		//$dir_profile = "ProfilePicture";
+		//$dir_resume = "Resumes";
+	
 	}
 
 	$connection = odbc_connect($dsn, $user, $password);
@@ -41,8 +44,10 @@
 		exit("Connection Failed: " . $connection);
 	}
 	
-	$db_col = 'Picture';
-	$db_col = 'ResumePath';
+	echo "<br/>connection established";
+	
+	$db_col = array ('Picture','ResumePath');
+	//$db_col = 'ResumePath';
 	
 	//$options['deletefile'] = 'resume';
 	/* if(strtolower($options['deletefile']) == 'profilepic'){
@@ -54,41 +59,42 @@
 		//$dir = "Resumes";
 		//echo "<br/>in deletefile : resume";
 	} */
- 
-	$sqlx="SELECT CONVERT(IMAGE ,CONVERT(VARCHAR(MAX) ,CONVERT(VARBINARY(MAX) ,".$db_col."))) AS  'UploadedFile', Id FROM   tblInstallUsers where  ".$db_col." !='' order by Id desc";
-	//echo "<br/>sql query : $sqlx"; 
-	$rsx=odbc_exec($connection,$sqlx);
 	$file_arr = array();
-	while (odbc_fetch_row($rsx))
-	{
-	    $user_id=odbc_result($rsx,"Id");
-		$UploadedFile=odbc_result($rsx,'UploadedFile');
-					   
-		if (strpos($UploadedFile, 'http://jmgroveconstruction.com') !== false) 
+	for($i=0 ; $i<2;$i++){
+		$sqlx="SELECT CONVERT(IMAGE ,CONVERT(VARCHAR(MAX) ,CONVERT(VARBINARY(MAX) ,".$db_col[$i]."))) AS  'UploadedFile', Id FROM   tblInstallUsers where  ".$db_col[$i]." !='' AND Id >3680 order by Id desc";
+		//echo "<br/>sql query : $sqlx"; 
+		$rsx=odbc_exec($connection,$sqlx);
+		
+		while (odbc_fetch_row($rsx))
 		{
-			echo "<br/>true";
-			$url_status = 1;
+			$user_id=odbc_result($rsx,"Id");
+			$UploadedFile=odbc_result($rsx,'UploadedFile');
+						   
+			if (strpos($UploadedFile, 'http://jmgroveconstruction.com') !== false) 
+			{
+				echo "<br/>true";
+				$url_status = 1;
+			}
+			else
+			{
+				echo "<br/>false";
+				$url_status = 0;
+			}
+						  
+			$ext_arr = explode("/", $UploadedFile);
+			$extension = end($ext_arr);
+						   
+			$user_url_status = $user_id. "_" .$url_status;
+			echo "<br/>user id : " . $user_id;
+			echo "<br/>user pic : " . $extension;
+			$file_arr[$i][$user_url_status] = $extension;
 		}
-		else
-		{
-			echo "<br/>false";
-			$url_status = 0;
-		}
-					  
-		$ext_arr = explode("/", $UploadedFile);
-		$extension = end($ext_arr);
-					   
-		$user_url_status = $user_id. "_" .$url_status;
-		echo "<br/>user id : " . $user_id;
-		echo "<br/>user pic : " . $extension;
-		$file_arr[$user_url_status] = $extension;
 	}
-	
 	echo "<pre>";
 	print_r($file_arr);
 	echo "<pre>"; 
 		 
-	
+	exit;
 	// Open Resumes directory, and read its contents
 	if (is_dir($dir_resume)){
 	    if ($dh = opendir($dir_resume)){
@@ -134,7 +140,10 @@
 		  }
 		}
 		
-		
+	
+	//PROFILE PICTURE RELATE CODE SSTARTS HERE
+	
+
 		
 		// Open ProfilePicture directory, and read its contents
 	if (is_dir($dir_profile)){
