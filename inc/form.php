fe62eae435ab8575a95a4ca79e0f7b6c74ac7dc5
Naveen <netcollab@users.noreply.github.com>
2017-10-24 15:15:52 +0530

    Quick Quote Upgrade

--- inc/form.php
+++ inc/form.php
@@ -1,5 +1,33 @@
 <?php
 
+include("../config.php");
+
+	error_reporting(0);
+	
+
+if($proocutonmode === 2)//
+{
+$dsn = "Live";
+}
+else if($proocutonmode === 1)
+{
+$dsn = "Test";
+}
+else{
+$dsn = "Driver={ODBC Driver 11 for SQL Server};Server=$serverName;Database=$database;";
+}
+
+//$dsn = "Driver={ODBC Driver 11 for SQL Server};Server=$serverName;Database=$database;";
+//$connection = odbc_connect("Driver={ODBC Driver 11 for SQL Server};Server=$serverName;Database=$database;", $user, $password);
+//$dsn = "Test";
+$connection = odbc_connect($dsn, $user, $password);
+if (!$connection)
+  {exit("Connection Failed: " . $connection);}
+  else
+  {
+	 
+  }
+
 //ini_set('display_errors','On');
 include('C:\Program Files (x86)\Parallels\Plesk\Additional\pleskphp5\pear\Mail.php');	
 
@@ -35,14 +63,14 @@ include('C:\Program Files (x86)\Parallels\Plesk\Additional\pleskphp5\pear\Mail.p
 	$message .= "<br> <strong>Address</strong>: ";
 	$message .= $_REQUEST["address"];
 	}
-	if(isset($_REQUEST['city']) && $_REQUEST['city']!= '') {
+	/* if(isset($_REQUEST['city']) && $_REQUEST['city']!= '') {
 	$message .= "<br> <strong>City, State</strong>: ";
 	$message .= $_REQUEST["city"];
 	$message .= " ";
 	}
 	if(isset($_REQUEST['state']) && $_REQUEST['state']!= '') {
 	$message .= $_REQUEST["state"];
-	}
+	} */
 	if(isset($_REQUEST['phone']) && $_REQUEST['phone']!= '') {
 	$message .= "<br> <strong>Phone</strong>: ";
 	$message .= $_REQUEST["phone"];
@@ -59,10 +87,27 @@ include('C:\Program Files (x86)\Parallels\Plesk\Additional\pleskphp5\pear\Mail.p
 	$message .= "<br> <strong>Email Address</strong>: ";
 	$message .= $_REQUEST["email"];
 	}
+	if(isset($_REQUEST['zip']) && $_REQUEST['zip']!= '') {
+	$message .= "<br> <strong>Zip Code</strong>: ";
+	$message .= $_REQUEST["zip"];
+	}
+	
+	if(isset($_REQUEST['product_line']) && $_REQUEST['product_line']!= '') {
+	$message .= "<br><br> <strong>Product Line </strong>: ";
+	$message .= $_REQUEST["product_line"]; 
+	}
+	
+	if(isset($_REQUEST['services_estimate']) && $_REQUEST['services-estimate']!= '') {
+	$message .= "<br><br> <strong>Service/Estimate </strong>: ";
+	$message .= $_REQUEST["services_estimate"]; 
+	}
+	
 	if(isset($_REQUEST['refer']) && $_REQUEST['refer']!= '')  {
 	$message .= "<br> <strong>Refered from</strong>: ";
 	$message .= $_REQUEST["refer"];
 	}
+	
+	
 	if(isset($_REQUEST['comments']) && $_REQUEST['comments']!= '') {
 	$message .= "<br><br> <strong>Comments</strong>: ";
 	$message .= $_REQUEST["comments"]; 
@@ -89,16 +134,25 @@ include('C:\Program Files (x86)\Parallels\Plesk\Additional\pleskphp5\pear\Mail.p
 		$subject			=	"Form Recieved on jmgroveconstruction.com $page_info";
 	}
 	
-	
+$recipients			=	'naveenkr.chaudhary82@gmail.com';
 
-$headers['From']	=	'service-sales@jmgroveconstruction.com'; //edit use same mail as used for smtp
-$recipients			=	'customer-service@jmgroveconstruction.com';
+/* $headers['From']	=	'service-sales@jmgroveconstruction.com'; //edit use same mail as used for smtp
+//$recipients			=	'customer-service@jmgroveconstruction.com';
+$recipients			=	'naveenkr.chaudhary82@gmail.com';
 
 $headers['To']		=	$recipients;
 $headers['Subject']	=	$subject;
 
 $headers['MIME-Version']	=	'1.0';
-$headers['Content-Type']	=	"text/html";
+$headers['Content-Type']	=	"text/html"; */
+
+$from_email = "service-sales@jmgroveconstruction.com";
+
+$headers = "From: <" . strip_tags($from_email)  . ">\r\n" . "CC: info@netcollab.in"; 
+
+
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 $body 						=	$message;
 	
@@ -108,7 +162,7 @@ $smtp_params["auth"]		=	true;
 $smtp_params["username"]	=	"service-sales@jmgroveconstruction.com"; // you made in plesk
 $smtp_params["password"]	=	"Azekdeck1"; //you set this in plesk for the email account
 
-$mail =& Mail::factory("smtp", $smtp_params);
+//$mail =& Mail::factory("smtp", $smtp_params);
 
 //print_r($headers);
 /*
@@ -127,16 +181,62 @@ if (!is_numeric($phone)) {
 	 echo "This ($phone) phone  is considered invalid.";
 exit;
 }
-$result = $mail->send($recipients, $headers, $body);
-		
+
+$customerName = $_REQUEST["fname"] . " " . $_REQUEST["lname"];
+$customerPhone = $_REQUEST["phone"];
+$customerEmail = $_REQUEST["email"];
+$customerZip = $_REQUEST["zip"];
+$productLine = $_REQUEST["product_line"];
+$servicesEstimate = $_REQUEST["services_estimate"];
+
+$pro_id = implode(',',$productLine);
+
+//$productLine = serialize($productLine1)
+
+//$sqlx='select * FROM dbo.tblProductMaster WHERE ";
+
+ /* $sqlx = 'SELECT * 
+          FROM dbo.tblProductMaster 
+         WHERE ProductId IN (' . $pro . ')';
+
+		 
+		$rsx = odbc_exec($connection,$sqlx);
+		$pname = array();		
+		while (odbc_fetch_row($rsx))
+		{
+		   //$productID=odbc_result($rsx,"ProductId");
+		   $productName=odbc_result($rsx,"ProductName");
+		   //echo "<hr>";
+		   $pname[] = $productName;
+		}
+$pro_names = implode(',',$pname);		
+	print_r($pro_names);  */
+//print_r($pro_id);
+
+$customer_data = "insert into dbo.new_customer (CustomerName, CellPh,Email, ZipCode, Service,ProductOfInterest) values ( '".$customerName."', '".$customerPhone."', '".$customerEmail."' , '".$customerZip."', '".$servicesEstimate."', '".$pro_id."')";
+
+
+$odbc_customer_data = odbc_prepare($connection, $customer_data);
+
+		if (!odbc_execute($odbc_customer_data)) { 
+		     
+		    echo "insert error in new_customer"; 
+			
+				exit;
+		}
+
+	$subject = "Free Project Quote";
+	//$result = $mail->send($recipients, $headers, $body);
+	$result = mail($recipients, $subject, $body, $headers);	
+	
 	if ($result) {	
 		$URL="http://www.jmgroveconstruction.com/quote-service-contact-us.php?message=sent";
-		//$URL="http://www.jmgroveconstruction.com/devlp/quote-service-contact-us.php?message=sent";
+		$URL="http://www.jmgroveconstruction.com/devlp/quote-service-contact-us.php?message=sent";
 		header ("Location: $URL");
 
  	} else {
  		$URL="http://www.jmgroveconstruction.com/quote-service-contact-us.php?message=failed";
- 		//$URL="http://www.jmgroveconstruction.com/devlp/quote-service-contact-us.php?message=failed";
+ 		$URL="http://www.jmgroveconstruction.com/devlp/quote-service-contact-us.php?message=failed";
 		header ("Location: $URL");
 	}
 	
--- inc/home-orm.php
+++ inc/home-orm.php
@@ -1,4 +1,35 @@
 <?php 
+
+include("config.php");
+
+	error_reporting(0);
+	
+
+if($proocutonmode === 2)//
+{
+$dsn = "Live";
+}
+else if($proocutonmode === 1)
+{
+$dsn = "Test";
+}
+else{
+$dsn = "Driver={ODBC Driver 11 for SQL Server};Server=$serverName;Database=$database;";
+}
+
+//$dsn = "Driver={ODBC Driver 11 for SQL Server};Server=$serverName;Database=$database;";
+//$connection = odbc_connect("Driver={ODBC Driver 11 for SQL Server};Server=$serverName;Database=$database;", $user, $password);
+//$dsn = "Test";
+$connection = odbc_connect($dsn, $user, $password);
+if (!$connection)
+  {exit("Connection Failed: " . $connection);}
+  else
+  {
+	 
+  }
+
+
+
 	//set some  url 
 	$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 	//$home_link = "http://$_SERVER[HTTP_HOST]";
@@ -64,10 +95,10 @@
 				return false;
 	  		}
 			
-			if (document.theForm2.city.value==null||document.theForm2.city.value==""){
+			/*if (document.theForm2.city.value==null||document.theForm2.city.value==""){
 	  			alert("Please enter your city.");
 				return false;
-	  		}
+	  		}*/
 			
 			if (document.theForm2.phone.value==null ||document.theForm2.phone.value==""){
 	  			alert("Please enter your phone number.");
@@ -97,11 +128,11 @@
 			}
 			
 			
-			 var response = grecaptcha.getResponse(widgetId1);
+			 /* var response = grecaptcha.getResponse(widgetId1);
            //recaptcha failed validation
            if(response.length == 0) {
 			   alert("Are you a machine?"); return false;
-		   }
+		   } */
 		   
 		   document.theForm2.action='inc/form.php';
 			$('#theForm2')[0].submit();
@@ -139,13 +170,21 @@
 	  		}
 		}
 	</script>
+	<style>
+	.form-required{color:red}
+	</style>
+	
+	<?php
+	
+	?>
+	
 	<h4 style="padding: 5px 20px; color:#FFF; font-size:1.1em; background:url(../img/main-header-bg.png) repeat-x scroll 0 0; -webkit-border-top-left-radius: 5px;
 -webkit-border-top-right-radius: 5px;
 -moz-border-radius-topleft: 5px;
 -moz-border-radius-topright: 5px;
 border-top-left-radius: 5px;
-border-top-right-radius: 5px; border-bottom:5px solid #000000;">
-		Quick Quote
+border-top-right-radius: 5px; border-bottom:5px solid #000000;background:#b94d4f">
+		Free Project Quote
 	</h4>
 <form id="theForm2" name="theForm2" method="post" onsubmit="return validate_form2(this)" action="inc/form.php" style="padding: 0 15px 0 15px;font-size:.8em;background:#efeeee none repeat scroll 0 0;">
 	  <input type="hidden" name="frmName" value="FreeConsultation">
@@ -158,11 +197,12 @@ border-top-right-radius: 5px; border-bottom:5px solid #000000;">
 		&nbsp;&nbsp;&nbsp;
 	<table cellspacing="10" cellpadding="10">
 			<tbody><tr>
-				<td><b>First Name:</b> <span title="This field is required." class="form-required">*</span></td><td><input type="Text" style="width:90px;" name="fname"></td>
+				<td colspan="2"><span class="form-required">* </span><input type="Text" style="width:94%;" name="fname" placeholder="First Name"></td>
 			</tr>
 			<tr>
-				<td><b>Last Name:</b> <span title="This field is required." class="form-required">*</span></td><td><input type="Text" style="width:90px;" name="lname"></td>
+				<td colspan="2"><span class="form-required">* </span><input type="Text" style="width:94%;" name="lname" placeholder="Last Name"></td>
 			</tr>
+			<!--
 			<tr>
 				<td><b>City &amp; State: </b> <span title="This field is required." class="form-required">*</span></td><td><input type="Text" style="width:70px;" name="city">&nbsp;
 				<select name="state">
@@ -220,29 +260,113 @@ border-top-right-radius: 5px; border-bottom:5px solid #000000;">
 				</select>
 				</td>
 			</tr>
+			-->
+			
 			<tr>
-				<td><b>Phone:</b> <span title="This field is required." class="form-required">*</span> </td><td><input type="Text" style="width:90px;" name="phone"></td>
+				<td colspan="2"><span class="form-required">* </span><input type="Text" style="width:94%;" name="email" placeholder="Email"></td>
+				
 			</tr>
 			<tr>
-				<td><b>Email:</b> <span title="This field is required." class="form-required">*</span></td><td><input type="Text" style="width:100px;" name="email"></td>
+				<td colspan="2"><span class="form-required">* </span><input type="Text" style="width:94%;" name="phone"  placeholder="Phone"></td>
+			</tr>
+			<tr>
+				<td colspan="2"><span class="form-required">* </span><input type="Text" style="width:94%;" name="zip"  placeholder="Zip"></td>
+				
+			</tr>
+			<tr>
+			<td colspan="2"><span class="form-required">* </span><b>Product Line:</b></td>
+			<tr>
+				<td colspan="2"><select style="width:100%;" name="product_line[]" multiple>
+				<option >select</option>
+				<?php
+				
+				$sqlx="select * FROM dbo.tblProductMaster ";
+				$rsx=odbc_exec($connection,$sqlx);
+				while (odbc_fetch_row($rsx))
+				{
+				   $productID=odbc_result($rsx,"ProductId");
+				   $productName=odbc_result($rsx,"ProductName");
+				 
+				?>
+				
+				<option value="<?php echo $productID ?>"><?php echo $productName ?></option>
+				
+				
+				<?php 
+				}
+				?>
+				</select>
+				</td>
+				
+			</tr>
+			
+			<tr>
+				<td colspan="2"><span class="form-required">* </span>
+				<input type="checkbox" id="coding" name="services_estimate" value="Services"> Services or 
+				<input type="checkbox" id="coding" name="services_estimate" value="Estimate"> Estimate
+				
+				
+				
+				</td>
 				
 			</tr>
 			<tr valign="top">
-				<td><b>Questions &amp; Comments:</b></td><td><textarea name="comments" style="width:100px;height:75px;"></textarea></td>
+				<td><b>Questions &amp; Comments:</b></td><td><textarea name="comments" style="width:100px;height:65px;"></textarea></td>
 			</tr>
 	    		
 			<tr style="display:none;">
 			<td><b>Fill Captcha:</b> <span title="This field is required." class="form-required">*</span></td><td><img style="margin-top:1px !important;" id="captcha2" src="inc/captcha_new.php"> <input type="text" value="" name="captcha_text" style="height:20px !important;  position: relative; top: -4px;">
 			<input type="button" style="background: url(inc/reload.gif); background-repeat: no-repeat; height:28px; width:24px; border:0px;position: relative; top: -7px;" id="reload2">
 			</td>					
-			</tr>		
+			</tr>
+			<div style="clear:both"></div>
+		
 		</tbody></table>
-				 <div id="example1"></div>
-		<p align="right">
+		<div id="example1"  style="clear:both"></div>
+		<p>
+		<input type="submit" value="send" style="width: 100px; height: 30px;">
 		<!--<input type="button" style="background: url(img/button-free-quote.jpg) repeat scroll 0 0 rgba(0, 0, 0, 0); height:38px; width:217px; border:0px; z-index: 2000; cursor: pointer;" onclick="validate_form2();" value="">-->
-		<input type="button" style="background: url(img/button-estimate.jpg) repeat scroll 0 0 rgba(0, 0, 0, 0); height:45px; width:200px; border:0px; z-index: 2000; cursor: pointer;" onclick="validate_form2('quote');" id="x_quote" value="" data-page="quote">
+	<!--	<input type="button" style="background: url(img/button-estimate.jpg) repeat scroll 0 0 rgba(0, 0, 0, 0); height:45px; width:200px; border:0px; z-index: 2000; cursor: pointer;" onclick="validate_form2('quote');" id="x_quote" value="" data-page="quote">
 		<input type="button" style="background: url(img/button-service.jpg) repeat scroll 0 0 rgba(0, 0, 0, 0); height:33px; width:200px; border:0px; z-index: 2000; cursor: pointer;" onclick="validate_form2('service');" id="x_service" value="" data-page="service">
+		-->
 		</p>
+		<div style="position:relative">
+		<p style="">
+ Already have an account? <a href="web.jmgrovebuildingsupply.com">Sign In</a> 
+</p>	
+<p>Or  log in with</p>
+<div style="text-align:center; display:inline-block; margin-top:2px; margin-left:15px;">
+                    <a style="margin:0px 2px;" href="#" target="_blank" rel="nofollow"><img src="img/social-icon/facebook-icon.png" width="24px" alt="Facebook" title="Facebook" /></a>
+                    <a style="margin:0px 2px;" href="#" target="_blank" rel="nofollow"><img src="img/social-icon/gmail-icon.png" width="24px" alt="Twitter" title="Twitter" /></a>
+                    <a style="margin:0px 2px;" href="#" target="_blank" rel="nofollow"><img src="img/social-icon/Instagram_icon.png" width="24px" alt="Twitter" title="Twitter" /></a>
+                    <a style="margin:0px 2px;" href="#" target="_blank" rel="nofollow"><img src="img/social-icon/twitter-icon.png" width="24px" alt="Twitter" title="Twitter" /></a>
+                    <a style="margin:0px 2px;" href="#" target="_blank" rel="nofollow"><img src="img/social-icon/snapchat.png" width="24px" alt="Twitter" title="Twitter" /></a>
+                    <!--<a style="margin:0px 2px;" href="#" onclick="return false;"><img src="img/social-icon/pintrest-icon.png" width="24px" alt="Pintrest" title="Pintrest" /></a>
+                    <a style="margin:0px 2px;" href="#" onclick="return false;"><img src="img/social-icon/youtube-icon.png" width="24px" alt="Youtube" title="Youtube" /></a>
+                -->
+				</div>
+				
+				</div>
+
+				 
+				  
+		
 			
 	</form>
-
+<script>
+$("input:checkbox").on('click', function() {
+  // in the handler, 'this' refers to the box clicked on
+  var $box = $(this);
+  if ($box.is(":checked")) {
+    // the name of the box is retrieved using the .attr() method
+    // as it is assumed and expected to be immutable
+    var group = "input:checkbox[name='" + $box.attr("name") + "']";
+    // the checked state of the group/box on the other hand will change
+    // and the current value is retrieved using .prop() method
+    $(group).prop("checked", false);
+    $box.prop("checked", true);
+  } else {
+    $box.prop("checked", false);
+  }
+});
+</script>
