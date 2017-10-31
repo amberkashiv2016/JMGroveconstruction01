1c3acd57fcaf130d4c1933acb68a0a26e1f390d4
Naveen <netcollab@users.noreply.github.com>
2017-10-31 10:32:08 +0530

    new footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-	<div id="footer-top">
+	<!--<div id="footer-top">
 		<div style="float:left;padding: 15px 18px 0px 18px;font-size:.8em;"><a href="about-us.php" style="text-decoration:none;">About Us</a></div>
 		<div style="float:left;"><img src="/img/footer-vdiv.jpg"></div>
 		<div style="float:left;padding: 15px 18px 0px 18px;font-size:.8em;"><a href="quote-service-contact-us.php?page=contact" style="text-decoration:none;">Contact Us</a></div>
@@ -10,14 +10,80 @@
         <div style="float:left;"><img src="/img/footer-vdiv.jpg"></div>
 		<div style="float:left;padding: 15px 18px 0px 18px;font-size:.8em;"><a href="quote-service-contact-us.php?page=contact" style="text-decoration:none;">Supplier & Vendor</a></div>
 		
-        
+     <div style="float:right;padding: 15px 18px 0px 18px;font-size:.8em;color:#777;">Copyright J.M. Grove. All Rights Reserved.</div>
+	</div> 
+-->	
         
 <?php /*?><div style="float:left;"><img src="/j/img/footer-vdiv.jpg"></div>
 <?php */?><?php /*?><div style="float:left;padding: 15px 18px 0px 18px;font-size:.8em;"><a href="employee-home.php" style="text-decoration:none;">Employees</a></div><?php */?>
 		<!---<div style="float:left;padding: 15px 18px 0px 18px;font-size:.8em;"><a href="financing.php" style="text-decoration:none;">Financing</a></div>--->
-		<div style="float:right;padding: 15px 18px 0px 18px;font-size:.8em;color:#777;">Copyright J.M. Grove. All Rights Reserved.</div>
+		
+	<div id="footer-section">
+	<div style="float:left;width:100%;height:24px;background: url(img/site-border.jpg) no-repeat;">
 	</div>
-	<div id="footer-bottom"></div>
+	<div id="footer-section1">
+		
+		<h4>Product & Sales</h4>
+		<p>Call: 1-555-5555 or <a href="jmgrove-chat.php" target="popup" onclick="window.open('jmgrove-chat.php','popup','width=600,height=600'); return false;"><span class="red">chat</span></a></p>
+		<p>General Customer Service</p>
+		<p>Call: 1-555-5551 or <a href="jmgrove-chat.php" target="_blank"><span class="red">chat</span></a></p>
+		<p>Check Order Status</p>
+		<p>Click <span class="red">here</span> for status</p>
+		<p>Check Install Status</p>
+		<p>Click <span class="red">here</span> for status</p>
+
+	</div>
+	<div id="footer-section2">
+		<h4>JMGrove Company Info</h4>
+		<a href="employment-jobs-careers-hiring-vacancies.php"><p>Employment</p></a>
+		<a href="about-us.php"><p>About us</p></a>
+		<a href="#"><p>JMGroveforPRO's</p></a>
+		<a href="#"><p>Suppliers & Vendors</p></a>
+		<a href="industry.php"><p>Industry Links</p></a>
+		<a href="#"><p>Site Directory</p></a>
+
+	</div>
+	<div id="footer-section3">
+	<h4>Services & Resources</h4>
+	<a href="#"><p>Installation Services</p></a>	
+<a href="#"><p>In-Store Services	</p></a>
+<a href="#"><p>JMGrove Newsroom</p></a>	
+<a href="#"><p>JMG Smart Homes	</p></a>
+<a href="#"><p>Innovative products</p></a>	
+<a href="#"><p>Liscense's, Qualifications & Insurance	</p></a>
+
+	</div>
+	<div id="footer-section4">
+	<h4>Customer Care</h4>
+	<a href="quote-service-contact-us.php?page=contact"><p>Contact Us/Find a Store</p></a>
+<a href="#"><p>Financing, cc & gift cards</p></a>
+<a href="#"><p>Shipping & Delivery</p></a>
+<a href="#"><p>Special Orders</p></a>
+<a href="#"><p>Returns/Exchanges</p></a>
+<a href="#"><p>Warranties/Rebates</p></a>
+
+	</div>
+	<script type="text/javascript" src="//platform-api.sharethis.com/js/sharethis.js#property=59f2f16458ca40001295592a&product=inline-share-buttons"></script>
+	
+	<div id="footer-section5">
+	<p>Get Latest, discounts, Rebates, How to, videos, and articles to your inbox</p>
+	<form action="" name="jmgsubscribe">
+		<input type="email" name="subscribe_email" id="subscribe_email" style="width:100%;margin-bottom:5px">
+		<input type="submit" name="subscribe_submit" id="subscribe_submit" value="subscribe" style="width:100%;background:#b94d4f;color:#fff;">
+	
+	</form>
+	<p>share the knowledge</p>
+	<div class="sharethis-inline-share-buttons"></div>
+	<p>Search Site, Articles, Videos, Training, ALL</p>
+	<form action="" name="jmgsearch">
+		<input type="text" name="jmgsearchtext" id="jmgsearchtext" style="width:140px;float:left">
+		<input type="submit" name="jmgsearch_submit" id="jmgsearch_submit" value="&nbsp;" >
+	
+	</form>
+	</div>
+	
+	</div>
+	<!--<div id="footer-bottom"></div> -->
 </div>
 <!---
 <div style="width:100%;padding:5px;float:left;">
